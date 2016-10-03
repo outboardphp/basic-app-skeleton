1d098676a5df45c453014d7f0c4e8406dcbb19e5
Garrett W <garrettw87@gmail.com>
2016-10-03 22:19:32 +0000

    moved DIC to its own repo

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 require "vendor/autoload.php";
 
-$container = new Aspire\Container(new Aspire\Config("aspire-app.json"));
+$container = new Aspire\DIC\Container(new Aspire\DIC\Config("aspire-app.json"));
 $app = $container->create("Aspire\\Framework");
 
-$app->input($_SERVER, $_GET, $_POST, $_COOKIE, $_FILES)->run();
+$app->input($_SERVER, $_GET, $_POST, $_COOKIE, $_SESSION, $_FILES)->run();
