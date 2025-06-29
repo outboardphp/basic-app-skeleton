ca4174da826671ecd22e391c0a42455b0669d828
garrettw <gw@garrettw.net>
2025-06-29 23:40:28 +0000

    manual dispatch only, for now

--- README.md
+++
@@ -1,2 +0,0 @@
-# aspire-app-skeleton
-Example files for a PHP app made with my Aspire framework.
--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require "vendor/autoload.php";
-
-$container = new Aspire\DIC\Container(new Aspire\DIC\Config("aspire-app.json"));
-$app = $container->create("Aspire\\Framework");
-
-$app->input($_SERVER, $_GET, $_POST, $_COOKIE, $_SESSION, $_FILES)->run();
